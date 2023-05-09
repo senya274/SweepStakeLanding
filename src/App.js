88afe08a8eeff698062861ec9534a63800c092cf
senya274 <senya2470@gmail.com>
2023-05-09 10:07:50 +0300

    refactor app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { BrowserRouter as Router, NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Main } from './Components/FirstTask/Main/Main';
-
 import { Description } from './Components/SecondTask/Description/Description';
 import { Menu } from './Components/Navigation/Menu';
-
-
-
-
 function App() {
   return (
     <Router>
       <div className="App">
        <Menu></Menu>
-     
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/Advertisement" element={<Description/>} />
