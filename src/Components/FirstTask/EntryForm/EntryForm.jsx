9db2867e2876450c48579bb890531808f764be18
senya274 <senya2470@gmail.com>
2023-05-09 10:12:47 +0300

    changed entryForm

--- src/Components/FirstTask/EntryForm/EntryForm.jsx
+++ src/Components/FirstTask/EntryForm/EntryForm.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
 import styles from "./EntryForm.module.css";
-
 function EntryForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-
   function handleSubmit(event) {
     event.preventDefault();
-
   }
-
   return (
     <div className={styles.form_container}>
-
       <form className={styles.form} onSubmit={handleSubmit}>
         <label>
           Имя:
@@ -23,8 +18,9 @@ function EntryForm() {
           <input className={styles.form_input} type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
         </label>
         <button className={styles.form_button} type="submit">Отправить</button>
-        <a className={styles.form_about} target="_blank" href="https://www.foxtrot.com.ua/uk/shop/piylesosiy_dyson_sv22_v15_dt_abs_eu.html?gclid=Cj0KCQjwu-KiBhCsARIsAPztUF2uxJpITospAq2R2WcnWULtVJQLwf9wZe7amOeUyIjatc2UJHxF-kUaAh6zEALw_wcB" className={styles.form_button}> Узнать больше</a>
-
+        <a className={styles.form_button} target="_blank" 
+        href="https://www.foxtrot.com.ua/uk/shop/piylesosiy_dyson_sv22_v15_dt_abs_eu.html?gclid=Cj0KCQjwu-KiBhCsARIsAPztUF2uxJpITospAq2R2WcnWULtVJQLwf9wZe7amOeUyIjatc2UJHxF-kUaAh6zEALw_wcB" 
+        >Узнать больше</a>
       </form>
     </div>
   );
